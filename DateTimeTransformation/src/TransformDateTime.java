455c87d0193a2759ac545c510eee45af5fdcb913
akshatsrivastav53 <79027239+akshatsrivastav53@users.noreply.github.com>
2021-03-24 20:32:29 -0500

    Update TransformDateTime.java
    New requirement - getting weekday for date

--- DateTimeTransformation/src/TransformDateTime.java
+++ DateTimeTransformation/src/TransformDateTime.java
@@ -26,9 +26,12 @@ public class TransformDateTime {
 
         public static void main(String[] args) throws ParseException {
                 //Test FormatDateandTimezone method
+        	    String testDateString1="03/03/2021 08:00:00";
         	    String testDateString2 = "24-03-2021 23:37:50";
         	    DateFormat df2 = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        	    DateFormat df1 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         	    Date dateTime = df2.parse(testDateString2);
+        	    Date dateTime1= df1.parse(testDateString1);
                 System.out.println("Date: " + dateTime);
                 System.out.println("Date in dd-MM-yyyy HH:mm:ss format is: "+df2.format(dateTime));
                 //Date dateTime = new Date();
@@ -38,6 +41,26 @@ public class TransformDateTime {
                 System.out.println("System Date in IST:"+FormatDateandTimezone(dateTime, "dd MMM yyyy hh:mm:ss a", "IST"));
                 System.out.println("System Date in GMT:"+FormatDateandTimezone(dateTime, "dd MMM yyyy hh:mm:ss a", "GMT"));
                 System.out.println("System Date in CST:"+FormatDateandTimezone(dateTime, "dd MMM yyyy hh:mm:ss a", "CST"));
+                System.out.println("getting week with Date time :"+getWeekOfDay(dateTime1));
         }
+// The below code is the part of new requirement to get week.
+		private static String getWeekOfDay(Date dateTime1) {
+			// TODO Auto-generated method stub
+			
+			Calendar cal = Calendar.getInstance();
+		    cal.setTime(dateTime1);
+		    String[] days = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+		    String week =days[cal.get(Calendar.DAY_OF_WEEK) - 1];
+		  
+		    DateFormat date = new SimpleDateFormat("MM/dd/yyyy");
+	        DateFormat time = new SimpleDateFormat("hh:mm:ss");
+	        
+	        String timeOnly = time.format(dateTime1);
+	        String dateOnly = date.format(dateTime1);
+	        
+	        String getWeek = dateOnly+" "+ week +" "+timeOnly;
+	       
+	        return getWeek;
+		}
 
 }
